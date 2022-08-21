75859dc9596bedee95a15179d49c0098133aa39a
Lapa <ksuhosha@mail.ru>
2022-08-21 12:19:21 +0300

    лекция 1

---
+++ Lecture_1/Examples05_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите имя пользователя:");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, Это же маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
