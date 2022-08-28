1135d60445a757a5b308dc8368ab813dd9310b48
Lapa <ksuhosha@mail.ru>
2022-08-28 11:52:45 +0300

    Лекция 3 таблица умнажения

---
+++ Lecture_3/examples_03/Program.cs
@@ -0,0 +1,9 @@
+﻿//задача вывод таблицы умнажения на экран
+
+ for(int i = 2; i <=10; i++){
+    for (int j = 0; j <=10; j++)
+    {
+        System.Console.WriteLine($"{i} x {j} = {i*j} ");
+    }
+    System.Console.WriteLine();
+ }
