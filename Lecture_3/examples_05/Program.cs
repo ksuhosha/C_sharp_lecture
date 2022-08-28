24d6090aaa1bcee6a7849045a62f656807fc2e4d
Lapa <ksuhosha@mail.ru>
2022-08-28 14:14:04 +0300

    Лекция 3 сортировка

---
+++ Lecture_3/examples_05/Program.cs
@@ -0,0 +1,38 @@
+﻿// Сортировка массива мин/макс
+
+int[] array = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+
+}
+
+Console.Clear();
+System.Console.WriteLine("без сортировки:");
+PrintArray(array);
+System.Console.WriteLine("отсортированый:");
+SelectionSort(array);
+PrintArray(array);
